7436c944b84f2f18277a1deba8c120fa93204571
brofegroy <threeusb@gmail.com>
2023-07-09 00:06:01 +0800

    shorterned some code in home screen controller, minor updates to blitzscreen documentation,everything still WIP

--- lib/screens/HomeScreen/home_screen.dart
+++ lib/screens/HomeScreen/home_screen.dart
@@ -15,73 +15,80 @@ class HomeScreen extends StatelessWidget {
     HomeScreenController controller = HomeScreenController(
       context: context,
     );
-    return Scaffold(
-      body: Center(
-        child: Container(
-          padding:
-              EdgeInsets.all(/*50*/ screenSize.width /10), ///[Hardcoded]
-          child: Column(
-            //
-            //
-            // --main screen column--
-            //
-            //
-            children: [
-              Image.asset("resources/images/title_screen_image.png"),
-              wordBlitzTitleLettersWidget(titleWidth: screenSize.width * 0.8 / 10),
-              Divider(),
-              ElevatedButton(
-                  onPressed: controller.handlePlayWordBlitz,
-                  child: const Text("Play WordBlitz")),
-              ValueListenableBuilder(
-                valueListenable: controller.continueBlitzScreenDataNotifier,
-                builder: (context,gameData,_) {
-                  return ElevatedButton(
-                      onPressed: (controller.isBlitzContinueEnabled)
-                          ? controller.handleContinueWordBlitz
-                          : null,
-                      child: const Text("Continue WordBlitz"));
-                }
+    return WillPopScope(
+      onWillPop: () async{return false;}, //disables back button in main screen, TODO make double tap back to exit
+      child: SafeArea(
+        child: Scaffold(
+          body: Center(
+            child: Container(
+              padding:
+                  EdgeInsets.all(/*50*/ screenSize.width /10), ///[Hardcoded]
+              child: Column(
+                //
+                //
+                // --main screen column--
+                //
+                //
+                children: [
+                  Image.asset("resources/images/title_screen_image.png"),
+                  wordBlitzTitleLettersWidget(titleWidth: screenSize.width * 0.8 / 10),
+                  Divider(),
+
+                  ElevatedButton(
+                      onPressed: (){controller.handlePlayWordBlitz(isContinuing: false);},
+                      child: const Text("Play WordBlitz")),
+                  ValueListenableBuilder(
+                    valueListenable: controller.continueBlitzScreenDataNotifier,
+                    builder: (context,gameData,_) {
+                      return ElevatedButton(
+                          onPressed: (controller.isBlitzContinueEnabled)
+                              ? (){controller.handlePlayWordBlitz(isContinuing: true);}
+                              : null,
+                          child: const Text("Continue WordBlitz"));
+                    }
+                  ),
+
+                  const ElevatedButton(
+                      onPressed: null,/*() async{
+                        var result = await Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                            builder: (context)=> const AnalysisScreen(
+                              gridLayout: ["G","Q","R","P",
+                                "G","Q","R","G",
+                                "G","Q","R","G",
+                                "G","Q","R","G",],
+                              initialList: ["BOY","YOG","YOGA","READER","WWWWWWWWWWWWWWWW","AAAAAAAAAAAAAAAA","IIIIIIIIIIIIIIII",
+                                "BOY","YOG","YOGA","READER","WWWWWWWWWWWWWWWW","ACCENT","IIIIIIIIIIIIIIII",
+                                "BOY","YOG","YOGA","READER","WWWWWWWWWWWWWWWW","ACCENT","IIIIIIIIIIIIIIII",
+                                "BOY","YOG","YOGA","READER","WWWWWWWWWWWWWWWW","ACCENT","IIIIIIIIIIIIIIII",
+                                "BOY","YOG","YOGA","READER","WWWWWWWWWWWWWWWW","ACCENT","IIIIIIIIIIIIIIII",
+                                "BOY","YOG","YOGA","READER","WWWWWWWWWWWWWWWW","ACCENT","IIIIIIIIIIIIIIII",],//temporarily here for debugging
+                            ))
+                        );
+                        print(result);
+                      },*/
+                      /// temporarily disabled for alpha release 1
+                      child: const Text("navigate to analysis")),
+                  const ElevatedButton(
+                      onPressed: null,/*() {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context)=> const SettingsScreen()
+                            )
+                        );
+                      },*/
+                      /// temporarily disabled for alpha release 1
+                      child: const Text("navigate to settings")),
+                ],
+                //
+                //
+                // --main screen column--
+                //
+                //
               ),
-              const ElevatedButton(
-                  onPressed: null,/*() async{
-                    var result = await Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                        builder: (context)=> const AnalysisScreen(
-                          gridLayout: ["G","Q","R","P",
-                            "G","Q","R","G",
-                            "G","Q","R","G",
-                            "G","Q","R","G",],
-                          initialList: ["BOY","YOG","YOGA","READER","WWWWWWWWWWWWWWWW","AAAAAAAAAAAAAAAA","IIIIIIIIIIIIIIII",
-                            "BOY","YOG","YOGA","READER","WWWWWWWWWWWWWWWW","ACCENT","IIIIIIIIIIIIIIII",
-                            "BOY","YOG","YOGA","READER","WWWWWWWWWWWWWWWW","ACCENT","IIIIIIIIIIIIIIII",
-                            "BOY","YOG","YOGA","READER","WWWWWWWWWWWWWWWW","ACCENT","IIIIIIIIIIIIIIII",
-                            "BOY","YOG","YOGA","READER","WWWWWWWWWWWWWWWW","ACCENT","IIIIIIIIIIIIIIII",
-                            "BOY","YOG","YOGA","READER","WWWWWWWWWWWWWWWW","ACCENT","IIIIIIIIIIIIIIII",],//temporarily here for debugging
-                        ))
-                    );
-                    print(result);
-                  },*/
-                  /// temporarily disabled for alpha release 1
-                  child: const Text("navigate to analysis")),
-              const ElevatedButton(
-                  onPressed: null,/*() {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context)=> const SettingsScreen()
-                        )
-                    );
-                  },*/
-                  /// temporarily disabled for alpha release 1
-                  child: const Text("navigate to settings")),
-            ],
-            //
-            //
-            // --main screen column--
-            //
-            //
+            ),
           ),
         ),
       ),
